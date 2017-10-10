527ea80e1a722f0b0f21b919fea5c7b7a8c3bebe
Malcolm Gorman <malcolm.gorman@gmail.com>
2017-10-10 11:12:02 +1100

    Revisiting FlatBuffers

--- flatbuffers.go
+++ flatbuffers.go
@@ -1,4 +1,27 @@
 package flatbuffers
 
-func whatever() {
+import (
+	"fmt"
+	"github.com/urban-wombat/gotables"
+	"runtime"
+	"path/filepath"
+	"strings"
+)
+
+func funcName() string {
+    pc, _, _, _ := runtime.Caller(1)
+    nameFull := runtime.FuncForPC(pc).Name() // main.foo
+    nameEnd := filepath.Ext(nameFull)        // .foo
+    name := strings.TrimPrefix(nameEnd, ".") // foo
+    return name
+}
+
+func MakeSchema(table *gotables.Table) (string, error) {
+	if table == nil {
+		return "", fmt.Errorf("%s(table): table is <nil>", funcName())
+	}
+
+	var schema string
+
+	return schema, nil
 }
